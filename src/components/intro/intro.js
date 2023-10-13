3bbe1a2e07a45e786e25e260b2f9dd42e6a19d7d
daggtes <valt.tesfaye@gmail.com>
2023-10-13 14:27:42 +0300

    new

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './intro.css';
-
+import bg from '../intro/intro image.png';
+import btnImg from '../intro/hire.png';
+import { Link } from "react-scroll";
 const Intro = () => {
     return (
          <section id="intro">
              <div className="introContent">
-
+                 <span className="hello">Hello</span>
+                 <span className="introText">I'm <span className="introName">Dagim</span> <br /> Website Designer</span>
+                 <p className="introPara"> I am a skilled web Designer with creating visually appealing and user friendly Website</p>
+                 <Link><button className="btn"><img src='' alt="" />Hire Me</button></Link>
             </div> 
             <img src={bg} alt="Profile" className="bg"/>
         </section>
